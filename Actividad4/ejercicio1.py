0be09833614a352bcb57f7c8543fd4311687f59e
EduMadriz20 <epmadriz@gmail.com>
2025-04-21 11:42:58 -0600

    Se valida que el rango_incial sea menor que el rango_final

--- Actividad4/ejercicio1.py
+++ Actividad4/ejercicio1.py
@@ -1,8 +1,12 @@
 rango_inicial = int(input("Escribe el inicio del rango: "))
 rango_final = int(input("Escribe el fin del rango: "))
 
-numero = int(input("Dime un numero: "))
-if(numero >= rango_inicial and numero <= rango_final):
-    print("El numero esta dentro del rnago.")
+if(rango_inicial < rango_final): 
+        numero = int(input("Dime un numero: "))
+        if(numero >= rango_inicial and numero <= rango_final):
+            print("El numero esta dentro del rnago.")
+        else: 
+            print ("El numero esta fuera del rango")
 else: 
-    print ("El numero esta fuera del rango")
+    print("EL rango inicial debe ser menor que el rango final")
+
